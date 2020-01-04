8556433373f80277413ade82af86648254927bca
yo_waka <y.wakahara@gmail.com>
2020-01-05 01:24:45 +0900

    remove comment

--- flux-utils-refactor/src/hooks/flux-utils.js
+++ flux-utils-refactor/src/hooks/flux-utils.js
@@ -2,13 +2,10 @@
 
 import { useEffect, useReducer } from 'react';
 
-// const batchDispatch = (stores, dispatch) => stores.forEach(store => dispatch(store));
-
 const useFluxStores = (stores, reducer, deps = []) => {
   const [state, dispatch] = useReducer(reducer, reducer());
 
   useEffect(() => {
-    //const listener = () => batchDispatch(stores, dispatch);
     const tokens = stores.map(store => store.addListener(() => {
       dispatch(store);
     }));
